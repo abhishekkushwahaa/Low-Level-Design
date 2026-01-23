052955aac66d615066dc8e4f140b84e3db030cb1
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2026-01-23 22:04:23 +0530

    singleton-safe

--- Design-Patterns/singleton.go
+++ Design-Patterns/singleton.go
@@ -1,5 +1,7 @@
 package main
 
+import "sync"
+
 type Singleton struct{}
 
 var instance *Singleton
@@ -10,6 +12,20 @@ var instance *Singleton
 // b)- Race conditions :- A race condition is a software bug where the outcome of a program depends on the unpredictable timing or sequence of multiple processes or threads accessing shared data, leading to inconsistent or incorrect results, like two users booking the last ticket simultaneously or bank transactions miscalculating balances. It happens in concurrent systems when operations aren't properly synchronized, creating a "race" to access and modify data, often with "check-then-act" logic failing due to intervening changes. Solutions involve using synchronization tools like locks or mutexes to control access, ensuring only one thread operates at a time.
 
 func GetInstance() *Singleton {
+	if instance == nil {
+		instance = &Singleton{}
+	}
+	return instance
+}
+
+// 2. Thread-safe Singleton
+// Problem: Slow (locks every call)
+var mu sync.Mutex
+
+func GetInstanceSafe() *Singleton {
+	mu.Lock()
+	defer mu.Unlock()
+
 	if instance == nil {
 		instance = &Singleton{}
 	}
