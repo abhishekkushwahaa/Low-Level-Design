b8fb83ff905a57d7eb4330daece8a81611970497
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2026-01-24 22:09:54 +0530

    singleton-double-check

--- Design-Patterns/Creational-Patterns/singleton.go
+++ Design-Patterns/Creational-Patterns/singleton.go
@@ -7,7 +7,8 @@ type Singleton struct{}
 var instance *Singleton
 
 // 1. Basic Singleton (lazy initialization)
-// Problem:
+// Benefits: Created only when needed (better memory usage)
+// Problems:
 // a)- Not thread-safe :- a piece of code, function, or data structure is not designed to work correctly when accessed by multiple threads simultaneously
 // b)- Race conditions :- A race condition is a software bug where the outcome of a program depends on the unpredictable timing or sequence of multiple processes or threads accessing shared data, leading to inconsistent or incorrect results, like two users booking the last ticket simultaneously or bank transactions miscalculating balances. It happens in concurrent systems when operations aren't properly synchronized, creating a "race" to access and modify data, often with "check-then-act" logic failing due to intervening changes. Solutions involve using synchronization tools like locks or mutexes to control access, ensuring only one thread operates at a time.
 
@@ -18,8 +19,16 @@ func GetInstance() *Singleton {
 	return instance
 }
 
-// 2. Thread-safe Singleton
-// Problem: Slow (locks every call)
+// 2. Eager Initialization
+// Benefits: Simple and safe
+// Problems: Not lazy - allocated whether used or not
+func GetEagerInstance() *Singleton {
+	return instance
+}
+
+// 3. Thread-safe Singleton
+// Benefits: Thread-safe
+// Problems: Slow (locks every call)
 var mu sync.Mutex
 
 func GetInstanceThreadSafe() *Singleton {
@@ -32,10 +41,9 @@ func GetInstanceThreadSafe() *Singleton {
 	return instance
 }
 
-// 3. Double-Checked Locking
+// 4. Double-Checked Locking
 // Benefits: a)- Faster than always locking b)- Safe in Go due to memory model (mostly)
 // Problems: a)- In some languages, unsafe without memory barriers b)- More complex logic
-
 func GetInstanceSafeDoubleCheck() *Singleton {
 	if instance == nil {
 		mu.Lock()
