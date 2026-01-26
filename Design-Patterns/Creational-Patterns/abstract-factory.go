896c316a3c5e097b025004c3693689d25c6b6d61
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2026-01-26 21:18:57 +0530

    abstract-factory

--- Design-Patterns/Creational-Patterns/abstract-factory.go
+++ Design-Patterns/Creational-Patterns/abstract-factory.go
@@ -1,1 +1,27 @@
 package main
+
+// Structure :-
+// Abstract Products → Interfaces (Payment, Refund)
+// Concrete Products → UPIFamily, UPIFamily
+// Abstract Factory → PaymentFactory
+// Concrete Factories → UPIFactory, CardFactory
+// Client → Uses only interfaces
+
+// Abstract Products
+type Payment interface {
+	Pay(amount int)
+}
+
+type Refund interface {
+	Process(amount int)
+}
+
+// Concrete Products - UPI Family
+
+// Concrete Products - UPI Family
+
+// Abstract Factory
+
+// Concrete Factories
+
+// Client
