3187a03889004ff9afe3d50d2df53694540bde1a
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2026-01-26 22:21:35 +0530

    simpler-builder

--- Design-Patterns/Creational-Patterns/builder.go
+++ Design-Patterns/Creational-Patterns/builder.go
@@ -3,11 +3,30 @@ package main
 type Computer struct {
 	CPU string
 	RAM string
-	SSD string
-	GPU string
 }
 
 // Builder
 type ComputerBuilder struct {
 	computer Computer
+}
+
+// 1. Simpler Builder (Fluent API)
+// Benefits: a)- Readable b)- Flexible c)- Avoids large constructor
+// Problems: a)- No strict construction order b)- Client controls all steps
+func NewComputerBuilder() *ComputerBuilder {
+	return &ComputerBuilder{}
+}
+
+func (b *ComputerBuilder) SetCPU(cpu string) *ComputerBuilder {
+	b.computer.CPU = cpu
+	return b
+}
+
+func (b *ComputerBuilder) SetRAM(ram string) *ComputerBuilder {
+	b.computer.RAM = ram
+	return b
+}
+
+func (b *ComputerBuilder) Build() Computer {
+	return b.computer
 }
