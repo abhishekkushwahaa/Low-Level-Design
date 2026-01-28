1e3187ec08ca4208c9d213e8c518617f1ec552bd
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2026-01-28 21:20:46 +0530

    builder-director

--- Design-Patterns/Creational-Patterns/builder.go
+++ Design-Patterns/Creational-Patterns/builder.go
@@ -30,3 +30,44 @@ func (b *ComputerBuilder) SetRAM(ram string) *ComputerBuilder {
 func (b *ComputerBuilder) Build() Computer {
 	return b.computer
 }
+
+// 2. Builder with Director
+// Benefits: a)- Separates construction from representation b)- Same process can build different objects
+// Problems: a)- More complex b)- Extra abstraction
+type ComputerBuilderDirector interface {
+	BuildCPU()
+	BuildRAM()
+	GetComputer() Computer
+}
+
+// Concrete Builder
+type GamingComputerBuilder struct {
+	computer Computer
+}
+
+func (g *GamingComputerBuilder) BuildCPU() {
+	g.computer.CPU = "Intel i9"
+}
+
+func (g *GamingComputerBuilder) BuildRAM() {
+	g.computer.RAM = "RTX 4090"
+}
+
+func (g *GamingComputerBuilder) GetComputer() Computer {
+	return g.computer
+}
+
+// Director
+type Director struct {
+	builder ComputerBuilderDirector
+}
+
+func NewDirector(b ComputerBuilderDirector) *Director {
+	return &Director{builder: b}
+}
+
+func (d *Director) Construct() Computer {
+	d.builder.BuildCPU()
+	d.builder.BuildRAM()
+	return d.builder.GetComputer()
+}
